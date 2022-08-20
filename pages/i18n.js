72f7e99efd860c718e759d53b29b155aa4bfb928
ajraduenz <aldoraduenzjunior@gmail.com>
2022-08-19 22:03:59 -0300

    update i18

--- pages/i18n.js
+++ pages/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+
 // don't want to use this?
 // have a look at the Quick start guide 
 // for passing in lng and translations on init
@@ -21,9 +21,9 @@ i18n
   .init({
     fallbackLng: 'en',
     debug: true,
-
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+    lng: 'en',
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json'
     }
   });
 
